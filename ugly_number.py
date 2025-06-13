b2bc43e07368565b341bf18926dfcfa109dfeba5
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-06-13 07:08:25 +0530

    ugly

--- ugly_number.py
+++ ugly_number.py
@@ -4,6 +4,9 @@
 # 1 (by definition, considered an ugly number)
 
 # 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 18, 20, 24, 25...
+# An ugly number is defined as a number whose only prime factors are 2, 3, or 5. 
+# This means that if the number can be divided by any other prime number, it is not considered an ugly number. 
+# Examples of ugly numbers include 1, 2, 3, 4, 5, 6, 8, 9, and 10. Non-examples are 7, 11, 14, and so on.
 
 def is_ugly(n):
     if n <= 0:
